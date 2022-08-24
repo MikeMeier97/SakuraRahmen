a432779ef0fd2f7fc08f241c6ab44f9372d058d6
MikeMeier97 <m0.0m58@yahoo.de>
2022-08-24 12:19:52 +0200

    Sakura Rahmen

---
+++ main.js
@@ -0,0 +1,22 @@
+function translateGerman () {
+    document.getElementById('menu-menu').innerHTML = "Menu";
+    document.getElementById('menu-location').innerHTML = "Unser Lokal";
+    document.getElementById('menu-about').innerHTML = "Über uns";
+    document.getElementById('titel2-head').innerHTML = "Die besten Nudeln in der Stadt!";
+    document.getElementById('howtoorder').innerHTML = "So bestellst du";
+    document.getElementById('order-noodle').innerHTML = "Wähle deine <br> Nudeln";
+    document.getElementById('order-broth').innerHTML = "Wähle deine <br> Brühe";
+    document.getElementById('add-toppings').innerHTML = "Wähle deine <br> Beilage";
+    document.getElementById('title-menu').innerHTML = "Unser Menü";
+    document.getElementById('chicken-table1').innerHTML = "Hähnchen";
+    document.getElementById('chicken-table2').innerHTML = "Hähnchen";
+    document.getElementById('chicken-table3').innerHTML = "Hähnchen";
+    document.getElementById('beef-table1').innerHTML = "Rinderfleisch";
+    document.getElementById('beef-table2').innerHTML = "Rinderfleisch";
+    document.getElementById('beef-table3').innerHTML = "Rinderfleisch";
+    document.getElementById('shrimp-table1').innerHTML = "Schrimps";
+    document.getElementById('shrimp-table2').innerHTML = "Schrimps";
+    document.getElementById('shrimp-table3').innerHTML = "Schrimps";
+    document.getElementById('find-us-title').innerHTML = "DU FINDEST UNS";
+
+}
